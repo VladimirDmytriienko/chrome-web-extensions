db3b0099a3cd1faca5eead70d83cb96649c6d935
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2025-03-06 01:11:46 +0200

    interaction with web page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { Button } from './components/ui/button'
 
 
 function App() {
 
+  const onclick = async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+    if (tab.id) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          document.body.style.backgroundColor = 'green';
+          alert('alertalertalert');
+        }
+      });
+    }
+  }
   return (
-    <>
+    <div className="= h-100 grid place-items-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       CHROME WEB EXTENSION
-    </>
+      <Button onClick={onclick}>alert</Button>
+    </div>
   )
 }
 
